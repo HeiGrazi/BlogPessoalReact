2433facd944560831e98f96ec65c872f8ff6ab09
HeiGrazi <trigiotti95@gmail.com>
2023-05-24 22:12:01 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
+import ListaTema from './componentes/temas/listatema/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './componentes/postagens/cadastropost/CadastroPost';
+import CadastroTema from './componentes/temas/cadastrotemas/CadastroTema';
+import DeletarPostagem from './componentes/postagens/deletarpostagem/DeletarPostagem';
+import DeletarTema from './componentes/temas/deletartemas/DeletarTema';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Home.css';
+import TabPostagem from '../../componentes/postagens/tabpostagem/TabPostagem';
+import { useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import ModalPostagem from '../../componentes/postagens/modalpostagem/ModalPostagem';
+
+function Home() {
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          navigate("/login")
+  
+      }
+  }, [token])
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalPostagem />
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src ="https://th.bing.com/th/id/OIP.3i5eudSOt9VxYa-vbY7oagHaEK?pid=ImgDet&rs=1" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default Home;
