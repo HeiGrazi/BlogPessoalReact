e82b9024fd3ffd9d643269c47e1850a53e30cd74
Graziella Santos <127500150+HeiGrazi@users.noreply.github.com>
2023-06-05 17:25:34 -0300

    Update DeletarPostagem.tsx

--- src/componentes/postagens/deletarpostagem/DeletarPostagem.tsx
+++ src/componentes/postagens/deletarpostagem/DeletarPostagem.tsx
@@ -7,6 +7,7 @@ import { buscaId, deleteId } from '../../../services/Service';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
 
 function DeletarPostagem() {
   let navigate = useNavigate();
@@ -18,7 +19,16 @@ function DeletarPostagem() {
 
   useEffect(() => {
       if (token == "") {
-          alert("Você precisa estar logado")
+        toast.error("Você precisa estar logado", {
+          position: "top-center",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+      });
           navigate("/login")
       }
   }, [token])
@@ -39,12 +49,21 @@ function DeletarPostagem() {
 
   function sim() {
     navigate('/postagens')
-      deleteId(`/postagens*3/${id}`, {
+      deleteId(`/postagens/${id}`, {
         headers: {
           'Authorization': token
         }
       });
-      alert('Postagem deletado com sucesso');
+      toast.success("Postagem deletada com sucesso", {
+        position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+    });
     }
   
     function nao() {
